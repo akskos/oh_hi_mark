42d1fddd08773a21227b544127f8f89f1eda3aed
Akseli Koskinen <koskinen.akseli@gmail.com>
2018-04-13 22:23:43 +0300

    position based event firing

--- src/script.js
+++ src/script.js
@@ -9,27 +9,42 @@ $(document).ready(function() {
     const events = [
 	{
 	    keyword: 'social networking',
-	    action: showSocialNetworkingZuck
+	    action: showSocialNetworkingZuck,
+	    active: false
 	}
     ];
 
     function fireEvents(element) {
-	events.forEach(e => {
+	for (let i = 0; i < events.length; i++) {
+	    const e = events[i];
 	    const text = element.text().toLowerCase();
-	    if (text.indexOf(e.keyword) !== -1) {
-		e.action();
-		return;
+	    const position = element.offset().top;
+	    const scrollTop = $(window).scrollTop();
+	    const windowHeight = $(window).height();
+	    function positionSatisfied() {
+		return position <= scrollTop + windowHeight / 2;
 	    }
-	});
+	    if (!e.active && positionSatisfied() && text.indexOf(e.keyword) !== -1) {
+		e.action();
+		e.active = true;
+		return false;
+	    }
+	}
     }
 
     function scanPageForKeywords() {
 	let textElements = $.merge($('p'), $(':header'));
 	textElements.each(function() {
-	    fireEvents($(this));
+	    return fireEvents($(this));
 	});
     }
 
-    console.log('jquery is ready');
-    scanPageForKeywords();
+    function scanLoop() {
+	setTimeout(function() {
+	    scanPageForKeywords();	
+	    scanLoop();
+	}, 10);
+    }
+
+    scanLoop();
 });
