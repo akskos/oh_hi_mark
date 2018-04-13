7c1156f45c4c1ca6a9da0717c967e96a2f1c19b9
Akseli Koskinen <koskinen.akseli@gmail.com>
2018-04-13 22:53:11 +0300

    add corner-zuck class to social-networking img

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     function showSocialNetworkingZuck() {
 	const url = chrome.extension.getURL('jesse.small.png');
-	$('body').prepend('<img id="social-networking" src="' + url + '" />');
+	$('body').prepend('<img class="corner-zuck" id="social-networking" src="' + url + '" />');
 	$('img#social-networking').hide();
 	$('img#social-networking').fadeIn(500);
     }
