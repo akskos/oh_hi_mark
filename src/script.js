c83df12282aefc1e2ecff1eb841f739b3c5b3178
Akseli Koskinen <koskinen.akseli@gmail.com>
2018-04-13 20:50:00 +0300

    social networking: fade in

--- src/script.js
+++ src/script.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     function showSocialNetworkingZuck() {
 	const url = chrome.extension.getURL('jesse.small.png');
 	$('body').prepend('<img id="social-networking" src="' + url + '" />');
+	$('img#social-networking').hide();
+	$('img#social-networking').fadeIn(500);
     }
 
     const events = [
