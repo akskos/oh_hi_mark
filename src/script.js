5cf9f0287913e3e6b57482ac31f55e8ada7a2bd3
Akseli Koskinen <koskinen.akseli@gmail.com>
2018-04-13 19:16:39 +0300

    social networking pic in the corner

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {
     console.log('jquery is ready');
+    $('body').prepend('<img id="social-networking" src="' + chrome.extension.getURL('jesse.small.png') + '" />');
+    console.log(document.getElementsByTagName('body'));
 });
